9df4e1d5d4183808f5f5e8082feb802dab0df255
Jitendra Nigam <jatinder@Jitendra-Nigam.local>
2015-04-02 17:07:52 -0700

    sample config file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
-header('Content-Disposition: attachment; filename="MySettings.mobileconfig"');
+header('Content-Disposition: attachment; filename="sample.mobileconfig"');
 echo $mobileconfig;
 ?>
