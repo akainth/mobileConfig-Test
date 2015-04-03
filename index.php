1392015eb905e92721154b2bf17a306ac7762333
Jitendra Nigam <jatinder@Jitendra-Nigam.local>
2015-04-02 17:33:00 -0700

    small changes in php file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
+header('Content-type: application/x-apple-aspen-config; charset=utf-8');
 header('Content-Disposition: attachment; filename="sample.mobileconfig"');
 echo $mobileconfig;
 ?>
