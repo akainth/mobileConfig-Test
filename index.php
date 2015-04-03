5b527b7b1715347c564961a0b2a8c63e52549062
Jitendra Nigam <jatinder@Jitendra-Nigam.local>
2015-04-03 10:46:48 -0700

    config Change

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-type: application/x-apple-aspen-config; charset=utf-8');
-header('Content-Disposition: attachment; filename="sample.mobileconfig"');
+header('Content-Disposition: attachment; filename="MySettings.mobileconfig"');
 echo $mobileconfig;
 ?>
