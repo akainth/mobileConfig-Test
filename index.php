00ee9be9c6d4d8a51914691214f203540f4f8ffc
Jitendra Nigam <jatinder@Jitendra-Nigam.local>
2015-04-02 18:13:02 -0700

    htaccess added

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type: application/x-apple-aspen-config');
+header('Content-type: application/x-apple-aspen-config; charset=utf-8');
 header('Content-Disposition: attachment; filename="sample.mobileconfig"');
 echo $mobileconfig;
 ?>
