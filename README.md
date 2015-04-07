9de76811b92d13ac0a09bfc7b4abeff8e23d34b9
Jitendra Nigam <jatinder@Jitendra-Nigam.local>
2015-04-07 10:43:36 -0700

    with html

--- README.md
+++
@@ -1,2 +0,0 @@
-# mobileConfig-Test
-for mobile Config 
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-type: application/x-apple-aspen-config; charset=utf-8');
-header('Content-Disposition: attachment; filename="MySettings.mobileconfig"');
-echo $mobileconfig;
-?>
